838fa49abbf7b1c77dc952834e4dc0cbf497cf5a
TwizzaltJonah <Jonah@Jaylan.net>
2024-10-13 19:40:41 -0500

    added a main loop using pygame

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-print("Hello from DJ")
-print("Hello from Jonah")
-print("Hellow again from DJ")
+import pygame
+
+isRunning: bool = True
+
+SCREEN_WIDTH: int = 1600
+SCREEN_HEIGHT: int = 800
+
+def main():
+    start()
+    while isRunning:
+        mainLoop()
+    pygame.quit()
+
+def start():
+    pygame.init()
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def mainLoop():
+    global isRunning
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("quit")
+            isRunning = False
+
+if __name__ == '__main__':
+    main()
